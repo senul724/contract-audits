7e4ffe8c5a03127848c48d5a406fe681ce9c7892
Senul <hardlove.mrplus@gmail.com>
2023-02-13 09:35:52 +0530

    imports changed

--- contracts/SzeetaAdministratorV2.sol
+++ contracts/SzeetaAdministratorV2.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: LGPL-3.0-only
 pragma solidity ^0.8.0;
 
-import "./openzeppalin-utils/EIP712.sol";
+import "./openzeppalin-draft/utils/cryptography/EIP712.sol";
 import "./interfaces/IRewardNFT.sol";
 import "./interfaces/ISzeetaEventRewards.sol";
 import "./interfaces/IFactory.sol";
